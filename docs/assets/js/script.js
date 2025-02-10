e714435e95bfdb570571dfeea34bf3ee224f9f9c
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-02-10 14:26:09 +0800

    Not finished

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,25 +1,40 @@
 const taskList = document.querySelector(".tasks");
 const btn = document.querySelector("#btn");
 const task = document.querySelector("#task");
+const char_count_status = document.querySelector("#characters");
+const char_left_status = document.querySelector("#charLeft");
 
 btn.addEventListener("click", addTask);
+task.addEventListener("keydown", countCharacters);
 
 function getTask() {
   return task.value;
 }
 
 function addTask() {
-  const divWithComponents = addComponents();
-  appendTask(divWithComponents);
+  const div = document.createElement("div");
+  div.innerHTML = addComponents();
+  appendTask(div);
 }
 
 function addComponents(div) {
-  return `<div><p>${getTask()}</p> <button>${"Add"}</button> 
-          <button>${"Delete"}</button></div>`;
+  return `<p>${getTask()}</p> <section><button>${"Add"}</button> 
+          <button>${"Delete"}</button></section>`;
 }
 
 function appendTask(currentTask) {
-  console.log("Appended");
+  taskList.appendChild(currentTask);
+  task.value = "";
+  task.focus();
+}
 
-  taskList.innerHTML = currentTask;
+function countCharacters() {
+  const word = task.value.split("");
+  console.log(word);
+  char_count_status.innerHTML = `TOTAL CHARACTERS : ${word.length}`;
+  reduceCharacters(word.length);
+}
+
+function reduceCharacters(current_word_count) {
+  char_left_status.innerHTML = `CHARACTERS LEFT : ${50 - current_word_count}`;
 }
